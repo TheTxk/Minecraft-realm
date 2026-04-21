c2a99331ba191eacfe8b7378fc4dc1dec42fced3
TheTxk <116103149+TheTxk@users.noreply.github.com>
2026-04-21 14:05:59 +1000

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const CONFIG = {
   serverName: "2026.2 Meinkaft", // CHANGE THIS
   serverVersion: "@26.13", // CHANGE THIS
-  joinLink: "https://thetxk.github.io/Minecraft-realm/index.html", 
+  joinLink: "https://www.minecraft.net/en-us/open?inviteCode=As6BHL8KsGobpAQ", 
   heroText:
     "Welcome to our realm — a place for big builds, relaxed survival, community events, exploration, and chaotic late-night ideas that somehow become full projects.",
 
